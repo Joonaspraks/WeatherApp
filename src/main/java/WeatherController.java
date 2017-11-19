f91f7b0a7c07ce9262952f2b97263787bf8cc68b
joonaspraks <joonas.praks@profitsoftware.com>
2017-11-19 23:59:24 +0200

    Implemented main functionality

--- src/main/java/WeatherController.java
+++ src/main/java/WeatherController.java
@@ -2,6 +2,8 @@
 import Beans.Forecast.ForecastResponse;
 import Beans.Weather.WeatherResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -40,20 +42,24 @@ class WeatherController {
     List<WeatherExtremes> getReports(String call, String city) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         //Map<String, Object> bean = mapper.readValue(stream, Map.class);
-        ForecastResponse forecast = mapper.readValue(requester(call, city), ForecastResponse.class);
-        List<WeatherExtremes> extremes = new ArrayList<>();
-        for(int i=0; i<3; i++){
-            Beans.Forecast.List list = forecast.getList().get(i);
-            extremes.add(new WeatherExtremes(
-                    new Date(list.getDt()*1000),
-                    Math.round(toCelsius(list.getMain().getTempMax())),
-                    Math.round(toCelsius(list.getMain().getTempMin())),
-                    forecast.getCity().getName()
-                    ));
-        }
+        InputStream inputStr = requester(call, city);
+        if (inputStr != null) {
+            ForecastResponse forecast = mapper.readValue(inputStr, ForecastResponse.class);
+            List<WeatherExtremes> extremes = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                Beans.Forecast.List list = forecast.getList().get(i);
+                extremes.add(new WeatherExtremes(
+                        new Date(list.getDt() * 1000),
+                        Math.round(toCelsius(list.getMain().getTempMax())),
+                        Math.round(toCelsius(list.getMain().getTempMin())),
+                        forecast.getCity().getName()
+                ));
+            }
 
-        System.out.println(extremes.toString());
-        return extremes;
+            System.out.println(extremes.toString());
+            return extremes;
+        }
+        else return null;
     }
 
     InputStream requester(String call, String city) throws IOException {
@@ -61,7 +67,14 @@ class WeatherController {
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         connection.setRequestMethod("GET");
         connection.connect();
-        return connection.getInputStream();
+        try{
+            return connection.getInputStream();
+        }
+        catch(FileNotFoundException error){
+            System.out.println("The inserted 'city' does not exist");
+            return null;
+        }
+
     }
 
     double toCelsius(double kelvin){
--- src/main/java/start.java
+++ src/main/java/start.java
@@ -1,10 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 public class start {
     public static void main(String[] args) throws IOException {
         WeatherController controller = new WeatherController();
-        controller.getReports("forecast","helsinki");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose a method of your liking:");
+        System.out.println("'1' to read the target city from console");
+        System.out.println("'2' to read the target city from the text file");
+        int i=0;
+        while(i!=1&&i!=2){
+            if(sc.hasNextInt()){
+                i = sc.nextInt();
+                if(i!=1&&i!=2){
+                    System.out.println("Not a suggested number, please select again!");
+                }
+            }
+            else{
+                System.out.println("Not a number, please select again!");
+                sc.next();
+            }
+
+        }
+        String city = "";
+        List<WeatherExtremes> results = null;
+        if(i==1){
+            while(results==null) {
+                System.out.println("Enter a city of your choosing");
+                city = sc.next();
+                results = controller.getReports("forecast", city);
+            }
+        }
+        if(i==2){
+            BufferedReader br = new BufferedReader(new FileReader("src/main/file.txt"));
+            try {
+                //StringBuilder sb = new StringBuilder();
+                city = br.readLine();
+
+                /*while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                city = sb.toString();*/
+            } finally {
+                br.close();
+            }
+            results = controller.getReports("forecast", city);
+        }
+
+
+        /*controller.getReports("forecast","helsinki");
         controller.getReportNow("weather","madrid");
-        controller.getCityCoordinates("weather","tallinn");
+        controller.getCityCoordinates("weather","tallinn");*/
     }
 }
